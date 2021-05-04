bef8f21a531b62b6e900ecd03c89a9699d578288
Marco <marcobackman@protonmail.com>
2021-05-05 08:36:42 +0900

    Deleted test line and added MainFrame instance call from ui package

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-import java.lang.*;
+package classfile;
+
+import classfile.ui.MainFrame;
 
 class Main {
+
+  private void run() {
+      MainFrame mainFrameInstance = new MainFrame();
+  }
+
   public static void main (String[] args) {
-     System.out.println("This is a test");
+      Main mainInstance = new Main();
+      mainInstance.run();
   }
 }
