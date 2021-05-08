b381140f9f3f74119aff8ea8ceed8d4a4ba1feef
Marco <marcobackman@protonmail.com>
2021-05-08 19:53:19 +0900

    Removed useless comments and fixed indentation. Changed final variables to statics to prevent IamageIcon parameter taking as instance variable.

--- src/ui/IconSets.java
+++ src/ui/IconSets.java
@@ -5,9 +5,9 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class IconSets {
-    //Add image files to relaive directory.
-	private final String FRAME_LOGO = "../img/NAVYLOGO_02.png"; //must edit: do not use absolute directory location.
-	private final String LABEL_LOGO = "../img/LabelLogo_mini.png"; //must edit: do not use absolute directory location.
+    //Add image files relative to the directory.
+	private final static String FRAME_LOGO = "../img/NAVYLOGO_02.png";
+	private final static String LABEL_LOGO = "../img/LabelLogo_mini.png";
 
     //TODO Refactor - file not found detection needed.
 	static ImageIcon mainIcon = new ImageIcon(IconSets.FRAME_LOGO);
