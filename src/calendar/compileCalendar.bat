1ad1e8d1523a9cbc2fcbb12f87dbd264c44f577e
MarcoBackman <roni2006@hanmail.net>
2021-05-12 16:19:25 +0900

    Added pacakge level compile runner

---
+++ src/calendar/compileCalendar.bat
@@ -0,0 +1,7 @@
+@ECHO off
+
+::compiles calendar files
+javac -cp ..\..\bin -d ..\..\bin *.java
+echo "calendar files compilation done."
+
+PAUSE
---
+++ src/data/compileData.bat
@@ -0,0 +1,7 @@
+@ECHO off
+
+::compiles data files
+javac -cp ..\..\bin -d ..\..\bin *.java
+echo "data files compilation done."
+
+PAUSE
