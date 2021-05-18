edc8e508d6b416a191f2f0feb09d90f5286eb6a4
MarcoBackman <roni2006@hanmail.net>
2021-05-18 09:34:48 +0900

    Added day naming convension set

---
+++ src/calendar/DayCharacter.java
@@ -0,0 +1,16 @@
+package classfile.calendar;
+
+public class DayCharacter {
+    public static final String[] DAYS_IN_ENGLISH_LETTER
+       = {"S", "M", "T", "W", "H", "F", "A"};
+    public static final String[] DAYS_IN_ENGLISH_SHORT
+       = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    public static final String[] DAYS_IN_ENGLISH_LONG
+       = {"SUNDAY",
+          "MONDAY",
+          "TUESDAY",
+          "WEDNESDAY",
+          "THURSDAY",
+          "FRIDAY",
+          "SATURDAY"};
+}
